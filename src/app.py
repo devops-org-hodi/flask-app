d1ca48478458b31aa03af09dd9723b73cc745eec
hothaifa zoubi <hothaifazoubi@gmail.com>
2026-09-03 18:42:42 +0300

    add code:

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes and origins
+
+
+@app.route("/")
+def hello_world():
+  return {"message": "Hello, World!"}  # Returning JSON is common for CORS APIs
+
+
+if __name__ == "__main__":
+  # host="0.0.0.0" makes the server accessible from other devices on your network
+  app.run(host="0.0.0.0", port=5000 )
